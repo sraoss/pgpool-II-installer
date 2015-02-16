a7210e3b65fe347e03e0b5affd24886a165ea896
Nozomi Anzai <anzai@sraoss.co.jp>
2015-02-16 14:14:15 +0900

    Improve
    ex.)
    $ ./make_package.sh
    
    $ ls work/*.tar.gz
    work/installer2-pg93-3.4.1_rhel-6-x86_64.tar.gz  work/installer2-pg94-3.4.1_rhel-6-x86_64.tar.gz
    work/installer2-pg93-3.4.1_rhel-7-x86_64.tar.gz  work/installer2-pg94-3.4.1_rhel-7-x86_64.tar.gz

--- make_package.sh
+++ make_package.sh
@@ -4,8 +4,8 @@
 # configuration
 # ---------------------------------------------------------------------
 
-CENTOS_VERSION=6
-ARCHITECTURE=x86_64
+CENTOS_VERSION_ARR=(6 7)
+ARCHITECTURE_ARR=(x86_64)
 
 MAJOR_VERSION=3.4
 P_VERSION=3.4.1
@@ -16,48 +16,78 @@ A_VERSION=3.4.1
 A_RELEASE=2
 A_RPM_VERSION=${A_VERSION}-${A_RELEASE}
 
-PG_MAJOR_VERSION=9.4
-PG_MAJOR_VERSION_WO_DOT=${PG_MAJOR_VERSION/./}
+PG_MAJOR_VERSION_ARR=(9.3 9.4)
+
+FILES_ARR=(
+    COPYING
+    README.md
+    install.sh
+    installer.conf.sample
+    lib/
+    templates/
+    uninstall.sh
+)
 
 # ---------------------------------------------------------------------
-# prepare
+# function
 # ---------------------------------------------------------------------
 
-rm -rf editted/*
+function package()
+{
+    local _CENTOS_VERSION=$1
+    local _ARCHITECTURE=$2
+    local _PG_MAJOR_VERSION=$3
 
-# ---------------------------------------------------------------------
-# download RPMs
-# ---------------------------------------------------------------------
+    local _PG_MAJOR_VERSION_WO_DOT=${_PG_MAJOR_VERSION/./}
+    local _BASE_URL="http://www.pgpool.net/yum/rpms/${MAJOR_VERSION}/redhat/rhel-${_CENTOS_VERSION}-${_ARCHITECTURE}"
+    local _PGPOOL_PG=pgpool-II-pg${_PG_MAJOR_VERSION_WO_DOT}
 
-echo
-echo "- download RPM"
-echo
+    local _INSTALLER_DIR="installer2-pg${_PG_MAJOR_VERSION_WO_DOT}-${P_VERSION}_rhel-${CENTOS_VERSION}-${ARCHITECTURE}"
 
-BASE_URL=http://www.pgpool.net/yum/rpms/${MAJOR_VERSION}/redhat/rhel-${CENTOS_VERSION}-${ARCHITECTURE}/
-PGPOOL_PG=pgpool-II-pg${PG_MAJOR_VERSION_WO_DOT}
+    echo
+    echo "================================================================================"
+    echo "CentOS ${_CENTOS_VERSION} (${_ARCHITECTURE}) / PostgreSQL ${_PG_MAJOR_VERSION}"
+    echo "================================================================================"
+    echo
 
-rm -f *.rpm
-wget ${BASE_URL}/${PGPOOL_PG}-${P_RPM_VERSION}pgdg.rhel${CENTOS_VERSION}.${ARCHITECTURE}.rpm
-wget ${BASE_URL}/${PGPOOL_PG}-extensions-${P_RPM_VERSION}pgdg.rhel${CENTOS_VERSION}.${ARCHITECTURE}.rpm
-wget ${BASE_URL}/pgpoolAdmin-${A_RPM_VERSION}pgdg.rhel${CENTOS_VERSION}.noarch.rpm
+    mkdir ${_INSTALLER_DIR}
+    cd ${_INSTALLER_DIR}
 
-echo
-ls *.rpm
+    echo
+    echo "----"
+    echo "put files"
+    echo "----"
+    echo
 
-# ---------------------------------------------------------------------
-# get sources for pgpool for installer
-# ---------------------------------------------------------------------
+    for _FILE in ${FILES_ARR[@]}; do
+        echo "- ${_FILE}"
+        cp -rf ../../${_FILE} .
+    done
 
-echo
-echo "- create lib/versions.sh "
-echo
+    echo
+    echo "----"
+    echo "download RPM"
+    echo "----"
+    echo
 
-# scripts
-cat > lib/version.sh <<EOT
+    wget ${_BASE_URL}/${_PGPOOL_PG}-${P_RPM_VERSION}pgdg.rhel${_CENTOS_VERSION}.${_ARCHITECTURE}.rpm
+        wget ${_BASE_URL}/${_PGPOOL_PG}-extensions-${P_RPM_VERSION}pgdg.rhel${_CENTOS_VERSION}.${_ARCHITECTURE}.rpm
+    wget ${_BASE_URL}/pgpoolAdmin-${A_RPM_VERSION}pgdg.rhel${_CENTOS_VERSION}.noarch.rpm
+
+    echo
+
+    echo
+    echo "----"
+    echo "create lib/version.sh "
+    echo "----"
+    echo
+
+    # scripts
+    cat > lib/version.sh <<EOT
 #!/bin/sh
 
 # CentOS
-CENTOS_VERSION=${CENTOS_VERSION}
+CENTOS_VERSION=${_CENTOS_VERSION}
 
 # PostgreSQL
 PG_MAJOR_VERSION=${PG_MAJOR_VERSION}
@@ -72,4 +102,32 @@ A_VERSION=${A_VERSION}
 A_RELEASE=${A_RELEASE}
 EOT
 
-cat lib/version.sh
+    cat lib/version.sh
+
+    # ---------------------------------------------------------------------
+    # tar cfz
+    # ---------------------------------------------------------------------
+
+    cd ../
+    tar cfz ${_INSTALLER_DIR}.tar.gz ${_INSTALLER_DIR}
+
+    echo
+}
+
+# ---------------------------------------------------------------------
+# body
+# ---------------------------------------------------------------------
+
+rm -rf work
+mkdir work
+cd work
+
+for CENTOS_VERSION in ${CENTOS_VERSION_ARR[@]}; do
+    for ARCHITECTURE in ${ARCHITECTURE_ARR[@]}; do
+        for PG_MAJOR_VERSION in ${PG_MAJOR_VERSION_ARR[@]}; do
+            package ${CENTOS_VERSION} ${ARCHITECTURE} ${PG_MAJOR_VERSION}
+        done
+    done
+done
+
+ls *.tar.gz
